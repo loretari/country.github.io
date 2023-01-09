f7cd3c39f52b2bb22cd005bbcc6a2e75bb3d36e0
loretari <loretari@gmail.com>
2023-01-09 19:26:31 +0200

    init: add fetch and response.json

--- script.js
+++ script.js
@@ -25,56 +25,15 @@ const renderCountry = function (data, className = '') {
 }
 
 
-const getCountryAndNeighbour = function (country) {
+const getCountryData = function (country) {
+    fetch(`https://restcountries.com/v3.1/name/${country}`).then(response => response.json()
+        .then(data => renderCountry(data[0])));
 
-    // AJAX call country 1
-
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-    request.send();
-
-    request.addEventListener('load', function () {
-        const [data] = JSON.parse(this.responseText);
-        console.log(data); // use this to study the data you want to use
-
-        // render country 1
-        renderCountry(data);
-
-        // Get neighbour country (2);
-        // const neighbour = data.borders?.[1];
-        const neighbours = data.borders;
-neighbours.forEach(neighbour => {
-
-
-        if (!neighbours) return;
-
-        // AJAX call country 2
-        const request2 = new XMLHttpRequest();
-
-        request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
-        request2.send();
-
-        request2.addEventListener('load', function () {
-            const [data2] = JSON.parse(this.responseText);
-            console.log(data2);
-
-            renderCountry(data2, 'neighbour');
-        })
-})
-
-    });
 
 };
 
 // sample countries whose we want to display
-// getCountryData('portugal');
-// getCountryData('vatican city');
-// getCountryData('san marino');
-// getCountryData('spain');
-
-// getCountryAndNeighbour('lithuania');
-getCountryAndNeighbour('gibraltar');
-// getCountryAndNeighbour('italy');
+getCountryData('lithuania');
 
 
 
