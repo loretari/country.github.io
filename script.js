29fdbdd3767ecc93aa169c07db4a12501f94ac47
loretari <loretari@gmail.com>
2023-01-09 09:39:12 +0200

    init: add different countries

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const getCountryData = function (country) {
 
     const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/portugal`);
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
     request.send();
 
     request.addEventListener('load', function () {
@@ -17,16 +18,26 @@ const countriesContainer = document.querySelector('.countries');
         const html = `<article class="country">
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
-            <h3 class="country__name">data.name.official</h3>
-            <h4 class="country__region">data.region</h4>
+            <h3 class="country__name">${data.name.official}</h3>
+            <h4 class="country__region">${data.region}</h4>
             <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}</p>
             <p class="country__row"><span>🗣️</span>${languages}</p>
-            <p class="country__row"><span>💰</span>${currencies}</p>
+            <p class="country__row"><span>💰</span>${currencies[0].name}</p>
           </div>
         </article>`;
 
         countriesContainer.insertAdjacentHTML('beforeend', html);
         countriesContainer.style.opacity = 1;
-    })
+    });
 
+};
 
+// sample countries whose we want to display
+// getCountryData('portugal');
+// getCountryData('vatican city');
+// getCountryData('san marino');
+// getCountryData('spain');
+
+getCountryData('lithuania');
+getCountryData('latvia');
+getCountryData('estonia');
