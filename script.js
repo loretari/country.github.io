ddcba02924f73371ba6525f4f26ecc74281b477e
loretari <loretari@gmail.com>
2023-01-13 10:46:17 +0200

    init: returning values from Async Functions

--- script.js
+++ script.js
@@ -37,20 +37,41 @@ const getPosition = function () {
 };
 
   const whereAmI = async function () {
-      //Geolocation
-       const pos = await getPosition();
-     const {latitude: lat, longitude: lng} = pos.coords;
+      try {
+          //Geolocation
+          const pos = await getPosition();
+          const {latitude: lat, longitude: lng} = pos.coords;
 
-      //Reverse geocoding
-      const resGeo = await fetch(`https://geocode.xyz/${lat}, ${lng}?geoit=json&auth=144588167875121e15886605x125889`);
-      const dataGeo = await resGeo.json();
+          //Reverse geocoding
+          const resGeo = await fetch(`https://geocode.xyz/${lat}, ${lng}?geoit=json&auth=144588167875121e15886605x125889`);
+          if (!resGeo.ok) throw new Error('Problem getting location data!');
+          const dataGeo = await resGeo.json();
 
-      // Country data
-      const res = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.country}`);
-const data = await res.json();
-renderCountry(data[0]);
+          // Country data
+          const res = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.country}`);
+if (!res.ok) throw new Error('Problem getting country!')
+          const data = await res.json();
+          renderCountry(data[0]);
+
+          return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+      } catch (err) {
+console.error(`${err}`);
+renderError(`${err.message}`);
+
+      //Reject promise returned from async function
+          throw err;
+      };
+
   };
-  whereAmI()
+(async function () {
+    try {
+       const city = await whereAmI();
+       console.log(`${city}`);
+    } catch (err) {
+console.log(`${err.message}`);
+    }
+})();
+
 
 
 
